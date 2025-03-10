2bd5a883bde693a90d5566a043b517f7f7cfb301
Luciano Schiavone <luciano.schiavone@gmail.com>
2025-03-10 10:39:52 +0100

    modifiche

--- src/Panino.java
+++ src/Panino.java
@@ -1,30 +1,3 @@
 public class Panino {
-    private int codice;
-    private String nome;
-    private String ingredienti;
-    private double costo;
-    
-    public Panino(int codice, String nome, String ingredienti,double costo) {
-        this.codice = codice;
-        this.nome = nome;
-        this.ingredienti = ingredienti;
-        this.costo=costo;
-    }
-
-    public int getCodice() {
-        return codice;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getIngredienti() {
-        return ingredienti;
-    }
-
-    public double getCosto(){
-        return costo;
-    }
     
 }
